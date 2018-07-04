7f41bd060a6ddb507271b9f003ed34e7822d8beb
Raj31 <rajendra_dixit@yahoo.com>
2018-07-04 17:17:49 -0400

    campaign active - inactive and single campaign

--- api/routes/campaigns.js
+++ api/routes/campaigns.js
@@ -3,11 +3,34 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const Campaign = require('../models/campaign');
+const moment = require('moment');
 
 router.get('/', (req, res, next) => {
     //to get the value of additional query parameters, use below code for query string color
-    //const color = req.query.color;
-    Campaign.find()
+    let status = req.query.status;
+    let filterCriteria='';
+    console.log("Status is 11111:",status);
+    if(status==null){
+        status='active'
+    }
+    if(status=='active'){
+        filterCriteria = {
+            "enddate": {
+           "$gt" : moment().format()
+            }
+        }
+    }else{
+        filterCriteria = {
+            "enddate": {
+           "$lte" : new Date(moment().format())
+            }
+        }
+    }
+
+    console.log(filterCriteria);
+    
+    console.log("Status is:",status);
+    Campaign.find(filterCriteria)
         .select({'campaignname':1,'startdate':1,'enddate':1,'advertiser':1})
         .exec()
         .then(doc => {
@@ -59,6 +82,34 @@ router.post('/', (req, res, next) => {
 });//url, handler
 
 
+router.get('/:campaignId', (req, res, next) => {
+    //how to call
+    //http://localhost:3000/campaigns/5aef5fb1d786d03dd422b38c
+
+    const id = req.params.campaignId;
+    //res.status(200).json({
+      //  message: 'Handling GET request for id::'+ id
+    //});
+
+    Campaign.findById(id)
+    .exec()
+    .then(doc => {
+        console.log('From database:', doc);
+
+        if (doc) {
+            res.status(200).json(doc);
+        } else {
+            res.status(404).json({
+                message: "Id not found",
+                id:id
+            });
+        };
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: err });
+    })
+});
 
 
 module.exports = router;
